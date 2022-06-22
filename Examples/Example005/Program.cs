21f273307798278ea88955c74227eedf753e1f7d
Kirill <zyzywin@mail.ru>
2022-06-22 16:50:39 +0300

    Examples005

---
+++ Examples/Example005/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(1, 100);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 100);
+Console.WriteLine(numberB);
+int result = numberA+numberB;
+Console.WriteLine(result);
