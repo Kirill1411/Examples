7e91f3ee659a56eb1664bb84c29ad2d4ef797db8
Kirill <zyzywin@mail.ru>
2022-06-22 22:39:16 +0300

    Examples007

---
+++ Examples/Example007/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 1;
+int b = 4;
+int c = 12;
+int d = 3;
+int e = 9;
+int max = a;
+
+if(a>max)max = a;
+if(b>max)max = b;
+if(c>max)max = c;
+if(d>max)max = d;
+if(e>max)max = e;
+Console.Write("max= ");
+Console.WriteLine(max);
