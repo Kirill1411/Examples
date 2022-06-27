b06b2dcf94fd96f0cc10f13aaf3c9d59f5ee9ef1
Kirill <zyzywin@mail.ru>
2022-06-27 20:14:39 +0300

    xamle006

--- Examples/Example006/Program.cs
+++ Examples/Example006/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Введите ваше имя");
 string username = Console.ReadLine();
 
-if(username.ToLower() == "маша")
+if (username.ToLower() == "маша")
 {
     Console.WriteLine("Ура, это же Маша!");
 }
