260cfdcb7fe087e364709e8091fddd5e8f961a97
Kirill <zyzywin@mail.ru>
2022-06-27 22:53:45 +0300

    Example012

--- Examples/Example012/Program.cs
+++ Examples/Example012/Program.cs
@@ -1,11 +1,11 @@
 ﻿void FillArray(int[] collection)
 {
     int Length = collection.Length;
-    int index = 0;  
+    int index = 0;
     while (index < Length)
     {
-    collection[index] = new Random().Next(1, 10);
-    index++;
+        collection[index] = new Random().Next(1, 10);
+        index++;
     }
 }
 
@@ -20,5 +20,28 @@ void PrintArray(int[] col)
     }
 }
 int[] array = new int[10];
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
 FillArray(array);
 PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
+
