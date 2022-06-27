383937fc09599d73223c5408bd98faa2b0fc65c3
Kirill <zyzywin@mail.ru>
2022-06-27 21:37:27 +0300

    Example011

---
+++ Examples/Example011/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 1, 2, 3, 4, 5, 6, 8, 9, 7 };
+
+int n = array.Length;
+int find = 9;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
